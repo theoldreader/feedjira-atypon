90f5bdebfc4fcd762710d74b38d6d2c573cfe186
Jim Fiorato <jfiorato@gmail.com>
2017-06-07 09:10:08 -0500

    Allow Travis to use different handlers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/feedjira_atypon')
 require 'sample_feeds'
 
+SAXMachine.handler = ENV['HANDLER'].to_sym if ENV['HANDLER']
+
 RSpec.configure do |c|
   c.include SampleFeeds
 end
